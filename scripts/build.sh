2d02886fa043a66a69180fd41dde5d7b86e6b165
Jason O'Neil <jason.oneil@gmail.com>
2018-03-14 15:22:10 +0800

    Move webpack.config.js into config/webpack/ folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node_modules/.bin/webpack --mode production --config node_modules/cultureamp-front-end-scripts/config/webpack.config.js
+node_modules/.bin/webpack --mode production --config node_modules/cultureamp-front-end-scripts/config/webpack/webpack.config.js
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node_modules/.bin/webpack-dev-server --mode development --config node_modules/cultureamp-front-end-scripts/config/webpack.config.js
+node_modules/.bin/webpack-dev-server --mode development --config node_modules/cultureamp-front-end-scripts/config/webpack/webpack.config.js
