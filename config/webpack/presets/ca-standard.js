ddf4dbeb087deff87a115ec0242fe86fc48ecfd5
Jason O'Neil <jason.oneil@gmail.com>
2018-03-15 10:52:37 +0800

    Add sass/postcss preset
    For now I'm not including the ability to configure these via config files, but they
    would be easy enough to configure via modifying the loader settings.

--- config/webpack/presets/ca-standard.js
+++ config/webpack/presets/ca-standard.js
@@ -3,7 +3,7 @@
 const WebpackConfigMaker = require('../../../webpack-config-maker/');
 
 const caStandard = (wcm /*: WebpackConfigMaker */) => {
-  wcm.usePresets([require('./babel')]);
+  wcm.usePresets([require('./babel'), require('./sass-postcss')]);
 };
 
 module.exports = caStandard;
